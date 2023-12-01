37b2570064fe7cde72151a2ef1d63b6885f34cf1
mmaksi <mmaksi.dev@gmail.com>
2023-12-01 18:32:03 +0100

    Refactored the custom interceptor to work with any DTO

--- src/interceptors/serialize.intercept.ts
+++ src/interceptors/serialize.intercept.ts
@@ -7,13 +7,14 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data: any) => {
-        return plainToInstance(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           // Converts data to UserDto and returns only what is to be exposed
           excludeExtraneousValues: true,
         });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { SerializeInterceptor } from 'src/interceptors/serialize.intercept';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -30,7 +31,7 @@ export class UsersController {
     }
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const userId = Number(id);

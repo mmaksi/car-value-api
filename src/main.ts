76ab564a1195c5af9e5988c5f0e381853174b644
mmaksi <mmaksi.dev@gmail.com>
2023-12-01 11:45:13 +0100

    created entities and tested the users controller

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Removes values from the request body that don't match the dto
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

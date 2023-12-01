9f759a7245f03e3d42b309c9d27641cf22c0f186
mmaksi <mmaksi.dev@gmail.com>
2023-12-01 17:46:18 +0100

    added users controller functions to get, remove and update users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,55 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/users.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
+  async createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     try {
-      this.usersService.createUser(email, password);
-      return body;
+      const entity = await this.usersService.createUser(email, password);
+      return entity;
     } catch (error) {
       return { message: `failed to save the user ${error}` };
     }
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const userId = Number(id);
+    const user = await this.usersService.findOne(userId);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    return await this.usersService.findAllUsers(email);
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    const userId = Number(id);
+    return await this.usersService.removeUser(userId);
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const userId = Number(id);
+    return await this.usersService.updateUser(userId, body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { FindOneOptions, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  createUser(email, password) {
+  async createUser(email: string, password: string) {
     const user = this.repo.create({ email, password });
+    return await this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    return await this.repo.findOne({ where: { id } });
+  }
+
+  async findAllUsers(email: string) {
+    return await this.repo.find({ where: { email } });
+  }
+
+  async updateUser(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User not found`);
+    }
+    Object.assign(user, attrs);
+    // run the hooks
     return this.repo.save(user);
+    // The `update()` and `insert()` method works with objects and it doesn't run the entity hooks
+    // return await this.repo.update(id, attrs);
+  }
+
+  async removeUser(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User not found`);
+    }
+    // run the hooks
+    return this.repo.remove(user);
+    // The `delete()` method works with objects and it doesn't run the entity hooks
+    // return await this.repo.remove(id);
   }
 }

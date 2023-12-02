6d1742e155eed568bd5b8a24def61f8b3363d424
mmaksi <mmaksi.dev@gmail.com>
2023-12-02 20:21:15 +0100

    added a route to create reports in the database and guarded it with the global auth guard

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { createReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createReport(reportDto: createReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+}
